111eec25710d914c2942b165b2d1514124c32171
hiddevuijk <hdvuijk@gmail.com>
2017-06-20 16:18:28 +0200

    added vecmanip

---
+++ vecmanip.h
@@ -0,0 +1,133 @@
+#ifndef GUARD_vecmanip_h
+#define GUARD_vecmanip_h
+
+/* 
+ * some functions to manipulate std vectors
+*/
+
+
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// multiply all elements of a vec with a const
+void mult_by(std::vector<double>& v, double a)
+{
+	for(int i=0;i<v.size();++i)
+		v[i] *= a;
+}
+
+std::vector<double> mult(const std::vector<double>& v, double a)
+{
+	std::vector<double> out = v;
+	mult_by(out,a);
+	return out;
+}
+
+// returns the length of a vector
+double len_vec(const std::vector<double>& a)
+{
+	double s =0;
+	int n = a.size();
+	for( int i=0; i<n;++i)
+		s += a[i]*a[i];
+	return std::sqrt(s);
+}
+
+// normalize a vector to length m
+// default m=1/
+void normalize(std::vector<double>& a,double m = 1.)
+{
+	double l = len_vec(a);
+	int n = a.size();
+	for(int i=0;i<n;++i) {
+		a[i] /= l;
+		a[i] *= m;
+	}
+}
+
+// create a new vector with entries ci = ai + bi
+std::vector<double> add(const std::vector<double>& a,
+		const std::vector<double>& b)
+{
+	int s = a.size();
+	std::vector<double> c(s);
+	for( int i=0;i<s;++i)
+		c[i] = a[i] + b[i];
+	return c;
+
+}
+
+// add vector b to vector a
+void add_to(std::vector<double>& a,
+		const std::vector<double>& b)
+{
+	int s = a.size();
+	for( int i=0;i<s;++i)
+		a[i] += b[i];
+
+}
+
+// create new vector with entries ci = ai - bi
+std::vector<double> subtr(const std::vector<double>& a,
+		const std::vector<double>& b)
+{
+	int s = a.size();
+	std::vector<double> c(a);
+	for( int i=0;i<s;++i)
+		c[i] = a[i] - b[i];
+	return c;
+
+}
+
+// subtract vector b from a
+void subtr_of(std::vector<double>& a,
+		const std::vector<double>& b)
+{
+	int s = a.size();
+	for( int i=0;i<s;++i)
+		a[i] -= b[i];
+
+}
+
+// print vector using cout
+void print_vec(std::vector<double>& v,int N = 0)
+{
+	if (N==0) N = v.size();
+	for(int i=0;i<N;++i)
+		std::cout << v[i] << std::endl;
+}
+
+// write vector to a file named fileName.
+// writes values separated by newline
+// write N values, default N = v.size()
+void write_vec(std::vector<double> v,std::string fileName, int N = 0)
+{
+	if(N==0) N = v.size();
+	std::ofstream out;
+	out.open(fileName);
+	for(int i=0;i<N;++i)
+		out<<v[i] << '\n';
+}
+
+
+// write vector to a file named fileName.
+// writes values separated by newline
+// write N values, default N = v.size()
+void write_vec(std::vector<int> v,std::string fileName, int N = 0)
+{
+	if(N==0) N = v.size();
+	std::ofstream out;
+	out.open(fileName);
+	for(int i=0;i<N;++i)
+		out<<v[i] << '\n';
+}
+
+
+
+
+
+
+#endif
