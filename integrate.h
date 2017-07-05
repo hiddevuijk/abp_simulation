e740fab3817f036e3264a90b138d6045faf4399b
hiddevuijk <hdvuijk@gmail.com>
2017-07-05 15:50:24 +0200

    changed force calculation, particles stay in box

---
+++ integrate.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_integrate_h
+#define GUARD_integrate_h
+
+
+
+
+
+
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 
 #include "deriv.h"
 
@@ -14,21 +15,34 @@ int main()
 	int N = 2;
 	double Dt = 1./30;
 	double Dr = 1./10.;
+	double gamma = 1.;
 	double beta = 1.;
 	double eps = 0.;
 	double sigma = 0.;
+	double L = 1.;
 	int seed = 123456789;
 
-	Deriv deriv(N,Dt,Dr,beta,eps,sigma,seed);
+	Deriv deriv(N,L,Dt,Dr,gamma,beta,eps,sigma,seed);
+
+	default_random_engine gen;
+	uniform_real_distribution<double> dist(0.,L);
 	vector<vector<double> > r(N,vector<double>(3,1.));
 	vector<vector<double> > dr(N,vector<double>(3));
 	vector<vector<double> > p(N,vector<double>(3,0.1));
 	vector<vector<double> > dp(N,vector<double>(3));
 
-	for(int i=0;i<2;++i)
+	for(int i=0;i<N;i++) {
+		r[i][0] = dist(gen); 
+		r[i][1] = dist(gen);
+		r[i][2] = dist(gen);
+		p[i][0] = dist(gen);
+		p[i][1] = dist(gen);
+		p[i][2] = dist(gen);
+	}
+	for(int i=0;i<2000;++i){
 		deriv(r,dr,p,dp,0.1);
-	cout << r[0][0] << endl;
-
+		cout << r[0][0] << endl;
+	}
 	return 0;
 }
 
