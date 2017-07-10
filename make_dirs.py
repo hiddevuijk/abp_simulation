edcec5fac6adfd759cca73d3b279532ba0ed47a4
hiddevuijk <hdvuijk@gmail.com>
2017-07-10 16:29:58 +0200

    changed input paramaters

--- make_dirs.py
+++ make_dirs.py
@@ -3,32 +3,35 @@ import numpy as np
 
 N = 100;
 Dt = 1./1;
-Dr = 1./10.;
+Dr = 20.*Dt;
 gamma = 1.;
 beta = 1.;
 eps = 1.;
 sigma = 1.;
-L = 10.;
-seed = 123456789;
-dt = (1.e-5)*sigma*sigma/Dt;
-tf = 1e1;
-teq = 1.;
+seed = 123456780;
+d = 1.
+dt = (1.e-5)*d*d/Dt;
+tf = 0.;
+teq = 20.*d*d/Dt;
 
+rho = 0.2
+
+L = (N/rho)**(1/3.)
 
 name = ""
 infile = open("input.txt",'w')
 infile.write("N=\n%i\n" % N)
+infile.write("rho=\n%f\n" % rho)
 infile.write("Dt=\n%f\n" % Dt)
 infile.write("Dr=\n%f\n" % Dr)
 infile.write("gamma=\n%f\n" % gamma)
 infile.write("beta=\n%f\n" % beta)
 infile.write("eps=\n%f\n" % eps)
 infile.write("sigma=\n%f\n" % sigma)
-infile.write("L=\n%f\n" % L)
 infile.write("dt=\n%f\n" % dt)
 infile.write("tf=\n%f\n" % tf)
 infile.write("teq=\n%i\n" % teq)
-infile.write("sees=\n%i\n" % seed)
+infile.write("seed=\n%i\n" % seed)
 infile.write("name=\n" + name)
 
 infile.close()
