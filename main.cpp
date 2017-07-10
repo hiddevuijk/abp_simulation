c9284ca06afe58cd65ee281b37d16afc5d199566
hiddevuijk <hdvuijk@gmail.com>
2017-07-10 16:29:31 +0200

    main changed for pdist and new input

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 #include <string>
 #include <random>
 
+#include "read.h"
 #include "deriv.h"
 #include "integrate.h"
-#include "read.h"
-
+#include "pair_distribution.h"
 
 using namespace std;
 
 
-int main()
+int main(int argc, char *argv[])
 {
 
 	int N, seed;
 	double Dt,Dr,gamma,beta,eps,sigma,L,dt,tf,teq;
-
+	double rho;
 	// name of the output file
 	string name;
 
@@ -30,27 +30,32 @@ int main()
 	}
 
 	// read variables from input file
-	read_variables(N,Dt,Dr,gamma,beta,eps,sigma,L,dt,tf,teq,seed,name,input_name);
-
+	read_variables(N,rho,Dt,Dr,gamma,beta,eps,sigma,dt,tf,teq,seed,name,input_name);
+	L = pow(N/rho,1./3);
 	Deriv deriv(N,L,Dt,Dr,gamma,beta,eps,sigma,seed);
 
 	default_random_engine gen(seed);
 
-	vector<vector<double> > r(N,vector<double>(3));
-	vector<vector<double> > dr(N,vector<double>(3));
-	vector<vector<double> > p(N,vector<double>(3));
-	vector<vector<double> > dp(N,vector<double>(3));
+	vector<vector<double> > pdist_mat(nAvg);
+	for( int n =0;n<nAvg;n++) {
 
-	// start with random initial vectors
-	rand_vecs(r,N,3,-.5*L,.5*L,gen);
-	rand_vecs(p,N,3,-.5*,.5*L,gen,1.);
+		vector<vector<double> > r(N,vector<double>(3));
+		vector<vector<double> > dr(N,vector<double>(3));
+		vector<vector<double> > p(N,vector<double>(3));
+		vector<vector<double> > dp(N,vector<double>(3));
 
-	integrate(r,dr,p,dp,deriv,0,tf,dt);
-	cout << r[0][0] << endl;
+		// start with random initial vectors
+		rand_vecs(r,N,3,-.5*L,.5*L,gen);
+		rand_vecs(p,N,3,-.5*L,.5*L,gen,1.);
 
+		integrate(r,dr,p,dp,deriv,0,teq,dt);
+
+		
+		pdist_mat[n] = pair_distances(r,L);
+	}
+	write_vec(pdist,"pdist.dat");
 	return 0;
 }
-
 
 
 
