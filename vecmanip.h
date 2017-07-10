4550754422882ebce99a95db3c0f73222b98e9f4
hiddevuijk <hdvuijk@gmail.com>
2017-07-10 17:08:19 +0200

    added write matrix

--- vecmanip.h
+++ vecmanip.h
@@ -147,6 +147,24 @@ void write_vec(std::vector<int> v,std::string fileName, int N = 0)
 }
 
 
+
+void write_mat(const std::vector<std::vector<double> >& m,int nv,
+	int nelem, const char outname[], char sep1 = '\n',char sep2 = ';')
+{
+
+	std::ofstream out;
+	out.open(outname);
+	for(int nvi=0;nvi<nv;nvi++){
+		for(int nelemi=0;nelemi<nelem;nelemi++){
+			out << m[nvi][nelemi];
+			if(nelemi + 1 < nelem) out << sep2;
+		}
+		out << sep1;
+	}
+}
+
+
+
 void rand_vecs(std::vector<std::vector<double> >& r,
 	int N, int dim, double min, double max,
 	std::default_random_engine& gen, double norm = -1.)
