48dd4184a24c88a34763f4b64a8ec81d7e66ec9f
hiddevuijk <hdvuijk@gmail.com>
2017-08-04 11:55:03 +0200

    corrected calculation

--- orientation.h
+++ orientation.h
@@ -12,12 +12,32 @@ void orientation(const std::vector<std::vector<double> >& r,
 	int N = r.size();
 	int j;
 	double x;
+
+	// keeps track of the number of particles in a bin
+	std::vector<double> pAvgN(pAvg.size(),0.0);
+
+	// set pAvg to zero
+	for(int i=0;i<pAvg.size();++i) {
+		pAvg[i][0] = 0;
+		pAvg[i][1] = 0;
+		pAvg[i][2] = 0;
+	}
+
 	for(int i=0;i<N;++i) {
 		x -= L*round(r[i][xyz]/L);
 		j = floor(x/dr);
-		pAvg[j][0] += p[i][0]/N;
-		pAvg[j][1] += p[i][1]/N;
-		pAvg[j][2] += p[i][2]/N;
+		pAvg[j][0] += p[i][0];
+		pAvg[j][1] += p[i][1];
+		pAvg[j][2] += p[i][2];
+
+		++pAvgN[j];
+	}
+
+	//Divide pAvg[i] by the number of particles in the bin
+	for(int i=0;i<pAvg.size();++i) {
+		pAvg[i][0] /= pAvgN[i];
+		pAvg[i][1] /= pAvgN[i];
+		pAvg[i][2] /= pAvgN[i];
 	}
 }
 
